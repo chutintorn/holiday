fac91fb6469056b4f1fb5284bd646e32f141832e
chutintorn.sri <zzz17244@gmail.com>
2025-05-26 13:30:05 +0700

    update API base URL to production

--- src/api/Booking.js
+++ src/api/Booking.js
@@ -1,8 +1,8 @@
 // src/services/flightService.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3100/flights';
-
+const API_BASE_URL = 'https://nodebasic-production.up.railway.app/flights';
+//const API_BASE_URL = 'http://localhost:3100/flights';
 /**
  * Get available flights
  * @param {Object} params - Flight search parameters
